2834d349167ccc684ddf8d08679261281db16f38
poup <bourquin.julien@gmail.com>
2016-01-30 00:03:02 +0100

    woops: need font for title

--- js/screens/title.js
+++ js/screens/title.js
@@ -19,7 +19,7 @@ game.TitleScreen = me.ScreenObject.extend({
             init : function() {
                 this._super(me.Renderable, 'init', [0, 0, me.game.viewport.width, me.game.viewport.height]);
                 // font for the scrolling text
-              //  this.font = new me.BitmapFont("32x32_font", 32);
+                this.font = new me.BitmapFont("32x32_font", 32);
 
                 // a tween to animate the arrow
                // this.scrollertween = new me.Tween(this).to({scrollerpos: -2200 }, 10000).onComplete(this.scrollover.bind(this)).start();
